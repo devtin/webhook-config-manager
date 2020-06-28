c355538a8bede575bc5594a63786a04608cd38ee
Martin Rafael Gonzalez <tin@devtin.io>
2020-06-28 14:21:09 -0400

    chore(release): 1.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+## 1.0.0 (2020-06-28)
+
+
+### Features
+
+* consolidates all webhook configs found ([f193720](https://github.com/devtin/webhook-config-manager/commit/f1937206644a146c7048b979390b292744610590))
---
+++ dist/webhook-config-manager.js
@@ -0,0 +1,39 @@
+/*!
+ * webhook-config-manager v1.0.0
+ * (c) 2020 Martin Rafael Gonzalez <tin@devtin.io>
+ * MIT
+ */
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var deepListDir = require('deep-list-dir');
+var path = _interopDefault(require('path'));
+var fs = _interopDefault(require('fs'));
+
+async function webhookConfigManager (directory) {
+  const hookFiles = await deepListDir.deepListDir(directory, { pattern: [/\/hook\.json$/, '!node_modules'] });
+  const hooks = [];
+  hookFiles.forEach(file => {
+    const payload = [].concat(require(file));
+    hooks.push(...payload);
+  });
+  return hooks
+}
+
+if (module.parent === null) {
+  if (process.argv.length > 4) {
+    throw new Error(`Usage: npx webhook-config-manager <directory=./> <destination=web-hooks.json>`)
+  }
+  const directory = path.resolve(process.cwd(), process.argv[2] || './');
+  const hookJson = path.resolve(process.cwd(), process.argv[3] || 'web-hooks.json');
+
+  webhookConfigManager(directory)
+    .then(config => {
+      fs.writeFileSync(hookJson, JSON.stringify(config, null, 2));
+    });
+}
+
+exports.webhookConfigManager = webhookConfigManager;
